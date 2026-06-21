e01f079a80341e5314613f16dba4ac340cc95ae7
Mahima Batheja <mbatheja@andrew.cmu.edu>
2026-06-20 23:54:54 -0400

    upgrade GitHub Actions to Node.js 24 compatible versions

--- chartdna_dbt/models/marts/dim_track.sql
+++ chartdna_dbt/models/marts/dim_track.sql
@@ -1,4 +1,6 @@
-select MAX(week_start) as last_week, track_name, artist_name, top_tags, url,
-       MAX(total_tag_count) as total_tag_count
+select MAX(week_start) as last_week, track_name, artist_name, 
+       ARG_MAX(top_tags, week_start) AS top_tags,
+       ARG_MAX(url, week_start) AS url,
+       ARG_MAX(total_tag_count, week_start) AS total_tag_count
   from {{ref('fct_chart_entries')}}
- group by track_name, artist_name, top_tags, url
+ group by track_name, artist_name
